f261cf0c3831e7ed890f35a6f6b31e815937570e
Jan Krutisch <jan@krutisch.de>
2009-01-11 23:01:13 +0100

    schema in git

---
+++ db/schema.rb
@@ -0,0 +1,50 @@
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
+# please use the migrations feature of Active Record to incrementally modify your database, and
+# then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your database schema. If you need
+# to create the application database on another system, you should be using db:schema:load, not running
+# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20090111191328) do
+
+  create_table "cached_feeds", :force => true do |t|
+    t.string   "href"
+    t.string   "title"
+    t.string   "link"
+    t.text     "feed_data"
+    t.string   "feed_data_type"
+    t.text     "http_headers"
+    t.datetime "last_retrieved"
+    t.integer  "time_to_live"
+    t.text     "serialized"
+  end
+
+  create_table "planet_feeds", :force => true do |t|
+    t.string   "title"
+    t.string   "feed_url"
+    t.string   "source_url"
+    t.datetime "accepted_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "login"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token"
+    t.integer  "login_count"
+    t.datetime "last_request_at"
+    t.datetime "last_login_at"
+    t.datetime "current_login_at"
+    t.string   "last_login_ip"
+    t.string   "current_login_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
