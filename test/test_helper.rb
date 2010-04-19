131c2a1a89e9a33c5d9ce20ae0f6ef6ffda34b03
Jan Krutisch <jan@krutisch.de>
2010-04-19 13:46:15 +0200

    upgrading to rails 2.3, added rwc banner

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path(File.dirname(__FILE__) + "/factories.rb")
 
 require 'mocha'
 
-class Test::Unit::TestCase
+class ActiveSupport::TestCase
   # Transactional fixtures accelerate your tests by wrapping each test method
   # in a transaction that's rolled back on completion.  This ensures that the
   # test database remains unchanged so your fixtures don't have to be reloaded
@@ -19,7 +19,7 @@ class Test::Unit::TestCase
   # don't care one way or the other, switching from MyISAM to InnoDB tables
   # is recommended.
   #
-  # The only drawback to using transactional fixtures is when you actually 
+  # The only drawback to using transactional fixtures is when you actually
   # need to test transactions.  Since your test is bracketed by a transaction,
   # any transactions started in your code will be automatically rolled back.
   self.use_transactional_fixtures = true
@@ -47,7 +47,7 @@ class Test::Unit::TestCase
     FeedTools::Feed.any_instance.stubs(:link).returns("http://www.example.com")
     FeedTools::Feed.any_instance.stubs(:feed_type).returns(:rss)
     FeedTools::Feed.any_instance.stubs(:title).returns("example feed")
-    
+
   end
-  
+
 end
